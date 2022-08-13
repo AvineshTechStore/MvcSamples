a0704a8ffef06ed7c2e65961d5b914a550737429
Gupta, Avinesh <Avinesh.Gupta@allscripts.com>
2022-08-14 01:05:25 +0530

    Api development

---
+++ App_Start/MappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MvcLearning.Dtos;
+using MvcLearning.Models;
+
+namespace MvcLearning.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        //install-package automapper -version:4.1
+        public MappingProfile()
+        {
+            // Domain to Dto
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<Movie, MovieDto>();
+
+
+            // Dto to Domain
+            Mapper.CreateMap<CustomerDto, Customer>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
+
+            Mapper.CreateMap<MovieDto, Movie>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
+        }
+    }
+}
---
+++ Dtos/CustomerDto.cs
@@ -0,0 +1,25 @@
+﻿using MvcLearning.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcLearning.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter customer's name.")]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+        //[Min18YearsIfAMember]
+        public DateTime? Birthdate { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+    }
+}
